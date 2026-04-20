e1170d2841fc04a8ead4c6a4fdf6e7a497f92f7c
dyarXP <dyarmigi@gmail.com>
2026-04-20 23:35:14 +0700

    Update Automation.lua

--- modules/Automation.lua
+++ modules/Automation.lua
@@ -2,24 +2,29 @@ return function(Tab, Fluent, Window)
     local Options = Fluent.Options
     local ReplicatedStorage = game:GetService("ReplicatedStorage")
     local LocalPlayer = game:GetService("Players").LocalPlayer
+    local workspace = game:GetService("Workspace")
 
-    -- Knit Setup
+    -- Knit Services Protection
     local Knit = require(ReplicatedStorage:WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.7.0"):WaitForChild("knit"))
     local KnitServices = Knit.Services
-    local PlaytimeRF = KnitServices.PlaytimeRewardService.RF.ClaimReward
 
-    Tab:AddSection("Farming System")
+    -- [[ SECTION: FARMING SYSTEM ]]
+    local FarmSection = Tab:AddSection("Farming System")
 
+    -- 1. Auto Claim Playtime
     Tab:AddToggle("AutoClaimPlaytime", { Title = "Auto Claim Playtime Rewards", Default = true })
     task.spawn(function()
         while true do
             task.wait(10)
             if Options.AutoClaimPlaytime and Options.AutoClaimPlaytime.Value then
-                for i = 1, 12 do pcall(function() PlaytimeRF:InvokeServer(i) end) end
+                pcall(function()
+                    KnitServices.PlaytimeRewardService.RF.ClaimReward:InvokeServer(1)
+                end)
             end
         end
     end)
 
+    -- 2. Auto Collect Money
     Tab:AddToggle("AutoCollectMoney", { Title = "Auto Collect Money (Plot 1-30)", Default = false })
     task.spawn(function()
         while true do
@@ -42,22 +47,29 @@ return function(Tab, Fluent, Window)
         end
     end)
 
+    -- [[ SECTION: BRAINROT ]]
     Tab:AddSection("Brainrot & Lucky Block")
 
+    -- 3. Auto Pickup
     Tab:AddToggle("AutoPickup", { Title = "Auto Pickup All (1-30)", Default = false })
     task.spawn(function()
         while true do
             task.wait(1)
             if Options.AutoPickup and Options.AutoPickup.Value then
-                for i = 1, 30 do pcall(function() KnitServices.ContainerService.RF.PickupBrainrot:InvokeServer(tostring(i)) end) end
+                for i = 1, 30 do
+                    pcall(function()
+                        KnitServices.ContainerService.RF.PickupBrainrot:InvokeServer(tostring(i))
+                    end)
+                end
             end
         end
     end)
 
+    -- 4. Auto Place Best
     Tab:AddToggle("AutoPlaceBest", { Title = "Auto Place Best Brainrot", Default = false })
     task.spawn(function()
         while true do
-            task.wait(2)
+            task.wait(1.5)
             if Options.AutoPlaceBest and Options.AutoPlaceBest.Value then 
                 pcall(function() KnitServices.ContainerService.RF.PlaceBest:InvokeServer() end) 
             end
